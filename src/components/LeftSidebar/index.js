b24414e8d3c3661ed84f4478277412876b3b4e69
Md Monirul Islam <mmislam027@gmail.com>
2022-02-19 22:25:00 +0600

    desing with responsive

--- src/components/LeftSidebar/index.js
+++ src/components/LeftSidebar/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
+import useInnerSize from '../../hook/useInnerSize';
 import Item from '../Items/Item';
-
 const LeftSidebar = ({ setSelected, items }) => {
+  const { width } = useInnerSize();
+  useEffect(() => {
+    console.log(width);
+  }, [width]);
   if (items.length < 0) return null;
   return (
     <div className='leftSidebar'>
       <div className='sliderItemsWrapper'>
         <Carousel
-          axis='vertical'
+          axis={width >= 768 ? 'vertical' : 'horizontal'}
           verticalSwipe='natural'
           interval={1000}
           autoPlay={true}
@@ -20,8 +24,8 @@ const LeftSidebar = ({ setSelected, items }) => {
           renderIndicator={false}
           showArrows={false}
           showStatus={false}
-          centerSlidePercentage={100 / 3}
-          thumbWidth={300}
+          centerSlidePercentage={width >= 768 ? 100 / 3 : 100}
+          //   thumbWidth={300}
         >
           {items.map((item) => (
             <div key={Math.random()} onClick={() => setSelected(item)}>
